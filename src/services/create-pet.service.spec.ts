0aa57b63e95d926d273c43e04f8fd25ad879c7cd
Norberto Boa <nboa.26@gmail.com>
2025-01-05 22:29:18 +0200

    test: test the feature of creating requirements on a new pet

--- src/services/create-pet.service.spec.ts
+++ src/services/create-pet.service.spec.ts
@@ -4,9 +4,11 @@ import { beforeEach, describe, expect, it } from "vitest";
 import { CreatePetService } from "./create-pet.service";
 import { hash } from "bcryptjs";
 import { ResourceNotFound } from "./errors/resource-not-found-error";
+import { InMemoryRequirementsRepository } from "@/repositories/in-memory/in-memory-requirements-repository";
 
 let petsRepository: InMemoryPetsRepository;
 let usersRepository: InMemoryUsersRepository;
+let requirementsRepository: InMemoryRequirementsRepository;
 let sut: CreatePetService;
 
 describe("Create Pet Service", () => {
@@ -14,7 +16,8 @@ describe("Create Pet Service", () => {
   beforeEach(() => {
     petsRepository = new InMemoryPetsRepository();
     usersRepository = new InMemoryUsersRepository();
-    sut = new CreatePetService(petsRepository, usersRepository);
+    requirementsRepository = new InMemoryRequirementsRepository();
+    sut = new CreatePetService(petsRepository, usersRepository, requirementsRepository);
   })
 
   it("Should be able to create a pet", async () => {
@@ -37,10 +40,37 @@ describe("Create Pet Service", () => {
       environment: "CLOSED",
       breed: "Buerbull",
       independent: "HIGH",
-      user_id: user.id
+      user_id: user.id,
     })
 
     expect(pet.id).toEqual(expect.any(String));
+  })
+
+  it("Should be able to create a pet with requirements", async () => {
+    const user = await usersRepository.create({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: await hash("password123", 6),
+      address: "123 Main St",
+      latitude: 40.7128,
+      longitude: -74.0060,
+      role: "ADMIN",
+      phone: "123-456-7890"
+    })
+
+    const { pet, requirements } = await sut.execute({
+      name: "Buddy",
+      age: "NEWBORN",
+      size: "MEDIUM",
+      energy_level: "MEDIUM",
+      environment: "CLOSED",
+      breed: "Buerbull",
+      independent: "HIGH",
+      user_id: user.id,
+      requirements: ["Open Space", "Lot of money"]
+    })
+
+    expect(requirements?.length).toEqual(2);
   })
 
   it("Should not be able to create a pet if user is invalid", async () => {

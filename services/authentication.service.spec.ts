b292da701440b898b15bde445b2955a201504f6d
Norberto Boa <nboa.26@gmail.com>
2025-01-05 03:32:10 +0200

    test: test user authentication with invalid password

--- services/authentication.service.spec.ts
+++ services/authentication.service.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, it } from "vitest";
 import { compare, hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 import { AuthenticationService } from "./authentication.service";
+import { InvalidCredentials } from "./errors/invalid-credentials-error";
 
 let usersRepository: InMemoryUsersRepository;
 let sut: AuthenticationService;
@@ -33,5 +34,26 @@ describe("Authentication Service", () => {
 
 
     expect(user.email).toEqual("john.doe@example.com");
+  })
+
+  it("should throw an error when password is invalid", async () => {
+    await usersRepository.create({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: await hash("password123", 6),
+      address: "123 Main St",
+      latitude: 40.7128,
+      longitude: -74.0060,
+      role: "ADMIN",
+      phone: "123-456-7890"
+    });
+
+    await expect(async () => {
+      await sut.execute({
+        email: "john.doe@example.com",
+        password: "12345"
+      })
+    }).rejects.toBeInstanceOf(InvalidCredentials);
+
   })
 })

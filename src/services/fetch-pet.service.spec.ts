20e3f3bf46a89deeccef5a88be3f8e37294f3eec
Norberto Boa <nboa.26@gmail.com>
2025-01-06 20:25:37 +0200

    test: test if throws error when city not provided on fetching pet

--- src/services/fetch-pet.service.spec.ts
+++ src/services/fetch-pet.service.spec.ts
@@ -2,6 +2,7 @@ import { beforeEach, describe, expect, it } from "vitest";
 import { InMemoryPetsRepository } from "@/repositories/in-memory/in-memory-pets-repository";
 import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
 import { FetchPetService } from "./fetch-pet.service";
+import { CityNotProvided } from "./errors/city-not-provided-error";
 
 let usersRepository: InMemoryUsersRepository;
 let petsRepository: InMemoryPetsRepository;
@@ -13,10 +14,8 @@ describe("Fetch Pet Service", () => {
     usersRepository = new InMemoryUsersRepository();
     petsRepository = new InMemoryPetsRepository(usersRepository);
     sut = new FetchPetService(petsRepository);
-  })
 
-  it("Should be fecth a pet", async () => {
-
+    // Creating user owner of pet
     await usersRepository.create({
       id: "user",
       name: "John Doe",
@@ -31,19 +30,20 @@ describe("Fetch Pet Service", () => {
       phone: "123-456-7890"
     });
 
-    await usersRepository.create({
-      id: "user-3",
-      name: "John Doe",
-      email: "john.doe@example.com",
-      password: "password123",
-      city: "Maputo",
-      neighborhood: "Sikwama",
-      province: "Matola",
-      latitude: 40.7128,
-      longitude: -74.0060,
-      role: "ADMIN",
-      phone: "123-456-7890"
+    // Creating In town pet
+    await petsRepository.create({
+      name: "In Town Pet",
+      age: "NEWBORN",
+      size: "MEDIUM",
+      energy_level: "MEDIUM",
+      environment: "CLOSED",
+      breed: "Buerbull",
+      independent: "HIGH",
+      user_id: "user"
     });
+  })
+
+  it("Should be fecth a pet", async () => {
 
     await usersRepository.create({
       id: "user-1",
@@ -60,28 +60,6 @@ describe("Fetch Pet Service", () => {
     });
 
     await petsRepository.create({
-      name: "In Town Pet",
-      age: "NEWBORN",
-      size: "MEDIUM",
-      energy_level: "MEDIUM",
-      environment: "CLOSED",
-      breed: "Buerbull",
-      independent: "HIGH",
-      user_id: "user"
-    });
-
-    await petsRepository.create({
-      name: "In Town Pet",
-      age: "NEWBORN",
-      size: "MEDIUM",
-      energy_level: "MEDIUM",
-      environment: "CLOSED",
-      breed: "Buerbull",
-      independent: "HIGH",
-      user_id: "user-3"
-    });
-
-    await petsRepository.create({
       name: "Off Town Pet",
       age: "NEWBORN",
       size: "MEDIUM",
@@ -94,15 +72,17 @@ describe("Fetch Pet Service", () => {
 
     const { pets } = await sut.execute({ city: "Maputo", page: 1 });
 
-    expect(pets.length).toEqual(2);
+    expect(pets.length).toEqual(1);
     expect(pets).toEqual([
       expect.objectContaining({
         name: "In Town Pet"
       }),
-      expect.objectContaining({
-        name: "In Town Pet"
-      })
     ])
   })
 
+  it("should not fetch a pet when city is empty", async () => {
+    await expect(async () => {
+      await sut.execute({ city: "", page: 1 });
+    }).rejects.toBeInstanceOf(CityNotProvided);
+  });
 })

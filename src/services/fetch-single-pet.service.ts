6b1b1ba40607ce2d81c6117fe32cc9d071c2dbda
Norberto Boa <nboa.26@gmail.com>
2025-01-06 21:20:02 +0200

    feat: create service that fetches single pet

---
+++ src/services/fetch-single-pet.service.ts
@@ -0,0 +1,25 @@
+import type { PetsRepository } from "@/repositories/pets-repository";
+import type { Pet } from "@prisma/client";
+import { ResourceNotFound } from "./errors/resource-not-found-error";
+
+interface fetchSinglePetServiceRequest {
+  id: string;
+}
+
+interface fetchSinglePetServiceResponse {
+  pet: Pet;
+}
+
+export class FetchSinglePetService {
+  constructor(private petsRepostory: PetsRepository) { };
+
+  async execute({ id }: fetchSinglePetServiceRequest) {
+    const pet = await this.petsRepostory.findById(id);
+
+    if (!pet) {
+      throw new ResourceNotFound();
+    }
+
+    return { pet };
+  }
+}
